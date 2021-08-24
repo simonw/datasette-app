0d1ee1dc3891de7e710e399de06dedca1eb10d80
Simon Willison <swillison@gmail.com>
2021-08-24 09:47:04 -0700

    Initial test using spectron and mocha, refs #9

---
+++ test/spec.js
@@ -0,0 +1,33 @@
+const Application = require("spectron").Application;
+const assert = require("assert");
+const electronPath = require("electron");
+const path = require("path");
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+describe("Application launch", function () {
+  this.timeout(10000);
+
+  beforeEach(async function () {
+    this.app = new Application({
+      path: electronPath,
+      args: [path.join(__dirname, "..")],
+    });
+    return this.app.start();
+  });
+
+  afterEach(function () {
+    if (this.app && this.app.isRunning()) {
+      return this.app.stop();
+    }
+  });
+
+  it("shows an initial window", async function () {
+    await sleep(1000);
+    return this.app.client.getWindowCount().then(function (count) {
+      assert.strictEqual(count, 1);
+    });
+  });
+});
