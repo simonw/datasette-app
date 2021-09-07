778789d1f7538b923851d137fb77b4021c9791ea
Simon Willison <swillison@gmail.com>
2021-09-07 14:35:34 -0700

    Edit menu for copy/paste, closes #59

--- main.js
+++ main.js
@@ -487,6 +487,44 @@ async function initializeApp() {
       ],
     },
     {
+      label: "Edit",
+      submenu: [
+        {
+          label: "Undo",
+          accelerator: "CmdOrCtrl+Z",
+          role: "undo",
+        },
+        {
+          label: "Redo",
+          accelerator: "Shift+CmdOrCtrl+Z",
+          role: "redo",
+        },
+        {
+          type: "separator",
+        },
+        {
+          label: "Cut",
+          accelerator: "CmdOrCtrl+X",
+          role: "cut",
+        },
+        {
+          label: "Copy",
+          accelerator: "CmdOrCtrl+C",
+          role: "copy",
+        },
+        {
+          label: "Paste",
+          accelerator: "CmdOrCtrl+V",
+          role: "paste",
+        },
+        {
+          label: "Select All",
+          accelerator: "CmdOrCtrl+A",
+          role: "selectAll",
+        },
+      ],
+    },
+    {
       label: "Navigate",
       submenu: [
         homeItem,
