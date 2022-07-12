6d5fbade0106cbc2b4a5787da20efdeccae07613
Simon Willison <swillison@gmail.com>
2022-07-12 14:30:41 -0700

    Record a video of the test, refs #133

--- test/spec.mjs
+++ test/spec.mjs
@@ -1,10 +1,18 @@
 import { test, expect } from '@playwright/test';
 import { _electron } from 'playwright';
 
+async function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 test('App launches and quits', async () => {
-  const app = await _electron.launch({ args: ['main.js'] });
+  const app = await _electron.launch({
+    args: ['main.js'],
+    recordVideo: {dir: 'test-videos'}
+  });
   const window = await app.firstWindow();
   await expect(await window.title()).toContain('Loading');
   await window.waitForSelector('#run-sql-link');
+  await sleep(1000);
   await app.close();
 });
