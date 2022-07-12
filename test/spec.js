50342faad316ece37c67fcd54b02775110f82e3e
Simon Willison <swillison@gmail.com>
2022-07-12 13:38:04 -0700

    Use Playwright, drop spectron
    Refs #133, #144

--- test/spec.js
+++ test/spec.js
@@ -1,34 +1,18 @@
-const Application = require("spectron").Application;
-const assert = require("assert");
-const electronPath = require("electron");
-const path = require("path");
-
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+const { _electron: electron } = require('playwright');
 
 describe("Application launch", function () {
   this.timeout(10000);
 
   beforeEach(async function () {
-    this.app = new Application({
-      path: electronPath,
-      args: [path.join(__dirname, "..")],
-    });
-    return this.app.start();
+    this.app = await electron.launch({ args: ['main.js'] });
   });
 
-  afterEach(function () {
-    if (this.app && this.app.isRunning()) {
-      return this.app.stop();
-    }
+  afterEach(async function () {
+    await this.app.close();
   });
 
   it("shows an initial window", async function () {
-    await sleep(1000);
-    this.app.client.getWindowCount().then(function (count) {
-      assert.strictEqual(count, 1);
-      done();
-    });
+    const window = await this.app.firstWindow();
+    await window.waitForSelector('#run-sql-link');
   });
 });
